f75481ce645df6d538431a78f18c7a72341528eb
tume1208 <tumenulzii1203@gmail.com>
2024-11-21 00:48:22 +0800

    Delete Screenshots directory

--- Screenshots/javaimg.js
+++
@@ -1,9 +0,0 @@
-
-        var Mainimg = document.getElementById("Mainimg");
-        var smallimg = document.getElementsByClassName("small-img");
-  
-       for (var i=0; i<smallimg.length; i++){
-        smallimg[i].onclick = function() {
-          Mainimg.src = this.src;
-        }
-       }
